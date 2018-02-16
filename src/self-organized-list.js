bf6572fdc931a5226ec191c9a17eb223e06fb2d6
Eikthyrnir <36528707+Eikthyrnir@users.noreply.github.com>
2018-02-16 19:24:54 +0200

    Update self-organized-list.js

--- src/self-organized-list.js
+++ src/self-organized-list.js
@@ -5,17 +5,6 @@ this.tail = null;
 }
 
 insert(data) {
-
-  var node = new Node(data);
-  if (this.length > 0) {
-  this.tail.next = node;
-  node.prev = this.tail;
-  this.tail = node;
-  } else {
-  this.head = node;
-  this.tail = this.head;
-  }
-  this.length++;
 }
 
 size() {
@@ -24,88 +13,23 @@ size() {
 }
 
 at(index) {
- 
-  var count = 0,
-  ReturnNode = this.head;
-  if (this.length == 0 || this.length < index || index < 0) {
-  return null;
-  } else {
-  for (; count < index; count++) {
-  ReturnNode = ReturnNode.next;
-  }
-  }
-  return ReturnNode.data;
+
 }
 
 findNode(data) {
- 
-  var ReturnNode = this.head;
-  while (ReturnNode != null) {
-  if (ReturnNode.data == data) {
-  return ReturnNode;
-  }
-  ReturnNode = ReturnNode.next;
-  }
-  return null;
+
 }
 
 toArray() {
- 
-  var ReturnNode = this.head,
-  ReturnArray = [];
-  while (ReturnNode != null) {
-  ReturnArray.push(ReturnNode.data);
-  ReturnNode = ReturnNode.next;
-  }
-  return ReturnArray;
+
 }
 
 removeAt(index) {
  
-  var count = 0,
-  node = this.head;
-  if (this.length == 1) {
-  this.head = null;
-  this.tail = null;
-  } else {
-  if (index == 0) {
-  this.head = this.head.next;
-  this.head.prev = null;
-  } else {
-  if (index == this.length - 1) {
-  this.tail = this.tail.prev;
-  this.tail.next = null;
-  } else {
-  for (; count < index; count++) {
-  node = node.next;
-  }
-  node.prev.next = node.next;
-  node.next.prev = node.prev;
-  }
-  }
-  }
-  this.length--;
 }
 
-  moveToFront(node) {
- 
-  moveToFront(node) { //тут код мне не нравится, но двигать дату через весь список идея еще хуже я её таки двигаю
-  var tempData=node.data;
-  while(node!=this.head){
-  node.data=node.prev.data;
-  node=node.prev;
-  }
-  this.head.data=tempData;
 }
 
-reorganize(data) {
- 
-  var tempNode = this.findNode(data);
-  if (tempNode) {
-  this.moveToFront(tempNode);
-  return true;
-  }
-  return false;
 }
 
 }
