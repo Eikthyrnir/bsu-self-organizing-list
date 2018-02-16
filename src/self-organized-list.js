d0a4b3b4434aeec2e56630b5cc159f11adc4a322
Eikthyrnir <36528707+Eikthyrnir@users.noreply.github.com>
2018-02-16 19:34:33 +0200

    Update self-organized-list.js

--- src/self-organized-list.js
+++ src/self-organized-list.js
@@ -1,4 +1,52 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
 
-lrfhwfmwei
-fwe
-wef
+class SelfOrganizedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insert(data) {
+
+    }
+
+    size() {
+        return this.length;
+    }
+
+    at(index) {
+
+    }
+
+    findNode(data) {
+
+    }
+
+    toArray() {
+
+    }
+
+    removeAt(index) {
+
+    }
+
+    moveToFront(node) {
+
+    }
+
+    reorganize(data) {
+
+    }
+
+}
+
+module.exports = {
+    SelfOrganizedList,
+    Node
+};
