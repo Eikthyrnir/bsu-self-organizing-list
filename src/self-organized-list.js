ed3021ce9faaaa3d012f6a1d9a9b1655dfab9828
Eikthyrnir <36528707+Eikthyrnir@users.noreply.github.com>
2018-02-18 17:08:44 +0200

    Update self-organized-list.js

--- src/self-organized-list.js
+++ src/self-organized-list.js
@@ -1,143 +1,133 @@
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-        this.prev = null;
-    }
+constructor(data) {
+this.data = data;
+this.next = null;
+this.prev = null;
+}
 }
 
 class SelfOrganizedList {
-    constructor() {
-        this.length=0;
-        this.head = null;
-        this.tail = null;
-    }
+constructor() {
+this.lenght=0;
+this.head = null;
+this.tail = null;
+}
 
-    insert(data) {
-        var a=new Node(data);
-        if(this.length==0) {
-            this.head = a;
-            this.tail = this.head;
-        }
-        else{
-            this.tail.next=a;
-            a.prev=this.tail;
-            this.tail=a;
-        }
-        this.length++;
-    }
+insert(data) {
+var node= new Node(data);
+if(this.lenght)
+{
+this.tail.next=node;
+node.prev=this.tail;
+this.tail=node;
 
-    size() {
-        return this.length;
-    }
+}
+else
+{
+this.head=node;
+this.tail=node;
+}
+this.lenght++;
+}
 
-    at(index) {
-        if(index<0||index>=this.length){
-            return null;
-        }
-        var i=0;
-        var a=this.head;
-        while(i<index){
-            a=a.next;
-            i++;
-        }
-        return a.data;
-    }
+size() {
+if(this.length===0){
+return 0;
+}
+return this.lenght;
+}
 
-    findNode(data) {
-        var i=0;
-        var a=this.head;
-        for(var i=0;i<this.length;i++){
-            if(a.data==data)
-                return a;
-            a=a.next;
-        }
+at(index) {
+var i=0;
+var node=this.head;
+if( this.lenght==0||index>this.lenght||index<0)
+{ return null;
 
-        return null;
-    }
+}
+if(index==0)
+return this.head.data;
+else
+{
+for(;i<index;i++)
+node=node.next;
+return node.data;
+}
+}
 
+findNode(data) {
+var r = this.head;
+for(var i=0;i<this.lenght;i++)
+{ if (r.data == data)
+{
+return r;
+}
+r = r.next;
+}
+return null;
+}
 
-    toArray() {
-        var arr=[];
-        var a=this.head;
-        for(var i=0;a;i++) {
-            arr[i]=a.data;
-            a=a.next;
-        }
-        return arr;
-    }
+toArray() {
+var pushnod=this.head;
+var masss = [];
+while (pushnod!= null) {
+masss.push(pushnod.data);
+pushnod = pushnod.next;
+}
+return masss;
+}
 
-    removeAt(index) {
-        if(index<0&&index>=this.length)
-            return;
+removeAt(index) {
+let n=this.head;
+let coun=0;
+if (index == this.length - 1)
+{
+this.tail = this.tail.prev;
+this.tail.next = null;
+}
+else{
+if (index == 0)
+{
+this.head = this.head.next;
+this.head.prev = null;
+}
+while(coun<index)
+{
+coun++;
+n=n.next;
+}
+n.next.prev = n.prev;
+n.prev.next = n.next;
+}
+this.length--;
+}
 
-        var a = this.head;
-        if(index==0){
-            this.head=this.head.next;
+moveToFront(node)
+{
+var tmp,zn=node.data;
+while(node!=this.head)
+{
+tmp = node.prev.data;
+node.prev.data = node.data;
+node.data = tmp;
+node=node.prev;
+}
+this.head.data=zn;
+}
 
-            if(this.length>1) {
-                this.head.prev = null;
-            }
-            else {
-                this.tail = null;
-            }
-        }else
-            if(index==this.length-1){
-                this.tail=this.tail.prev;
-                this.tail.next=null;
-            }
-            else {
-                for (var i = 0; i < index; i++) {
-                    a = a.next;
-                }
-
-                var before = a.prev;
-                var after = a.next;
-                before.next = after;
-                after.prev = before;
-            }
-        this.length--;
-    }
-
-    moveToFront(node) {
-        if(this.length==0)
-            return;
-
-        var a=this.head;
-        while(a){
-            if(a==node){
-                while(a!=this.head){
-                    var tmp=a.data;
-                    a.data=a.prev.data;
-                    a.prev.data=tmp;
-                    a=a.prev;
-                }
-                return;
-            }
-            a=a.next;
-        }
-    }
-
-    reorganize(data) {
-        if(this.length==0)
-            return false;
-
-        var i=0;
-        var a=this.head;
-        for(var i=0;i<this.length;i++){
-            if(a.data==data){
-                this.moveToFront(a);
-                return true;
-            }
-            a=a.next;
-        }
-
-        return false;  
-    }
+reorganize(data) {
+if(this.lenght==0)
+return false;
+if(this.findNode(data))
+{
+this.moveToFront(this.findNode(data));
+return true;
+}
+return false;
+}
 
 }
 
 module.exports = {
-    SelfOrganizedList,
-    Node
+SelfOrganizedList,
+Node
 };
