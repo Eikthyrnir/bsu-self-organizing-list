eea33db97ab663fb35548da1705aafc53df423b4
Eikthyrnir <36528707+Eikthyrnir@users.noreply.github.com>
2018-02-18 18:07:52 +0200

    Update self-organized-list.js

--- src/self-organized-list.js
+++ src/self-organized-list.js
@@ -82,7 +82,13 @@ class SelfOrganizedList {
         let coun = 0;
         if (index == this.length - 1) {
             this.tail = this.tail.prev;
-            this.tail.next = null;
+            
+            if(this.length>1) {
+                this.tail.next = null;
+            }
+            else {
+                this.head = null;
+            }
         }
         else
             if (index == 0) {
