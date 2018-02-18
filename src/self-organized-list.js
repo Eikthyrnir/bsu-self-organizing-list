8d84ffa812d063ded4b906cf26034557e66ccba6
Eikthyrnir <36528707+Eikthyrnir@users.noreply.github.com>
2018-02-18 18:16:39 +0200

    Update self-organized-list.js

--- src/self-organized-list.js
+++ src/self-organized-list.js
@@ -78,29 +78,27 @@ class SelfOrganizedList {
     }
 
     removeAt(index) {
-        let n = this.head;
-        let coun = 0;
-        if (index == this.length - 1) {
-            this.tail = this.tail.prev; 
-            if(this.length>1) {
-                this.tail.next = null;
-            }
-            else {
-                this.head = null;
-            }
+        let n=this.head;
+        let coun=0;
+        if (index == this.length - 1)
+        {
+            this.tail = this.tail.prev;
+            this.tail.next = null;
         }
-        else
-            if (index == 0) {
+        else{
+            if (index == 0)
+            {
                 this.head = this.head.next;
                 this.head.prev = null;
-            }else{
-                while (coun < index) {
-                    coun++;
-                    n = n.next;
-                }
-                n.next.prev = n.prev;
-                n.prev.next = n.next;
             }
+            while(coun<index)
+            {
+                coun++;
+                n=n.next;
+            }
+            n.next.prev = n.prev;
+            n.prev.next = n.next;
+        }
         this.length--;
     }
 
