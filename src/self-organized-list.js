fb4734e997e680a74b0aa6117f87845b5c5d6d26
Eikthyrnir <36528707+Eikthyrnir@users.noreply.github.com>
2018-02-18 17:19:42 +0200

    Update self-organized-list.js

--- src/self-organized-list.js
+++ src/self-organized-list.js
@@ -8,63 +8,72 @@ class Node {
 
 class SelfOrganizedList {
     constructor() {
-        this.length=0;
+        this.lenght=0;
         this.head = null;
         this.tail = null;
     }
 
     insert(data) {
-        var a=new Node(data);
-        if(this.length==0) {
-            this.head = a;
-            this.tail = this.head;
+        var node= new Node(data);
+        if(this.lenght)
+        {
+            this.tail.next=node;
+            node.prev=this.tail;
+            this.tail=node;
+
         }
-        else{
-            this.tail.next=a;
-            a.prev=this.tail;
-            this.tail=a;
+        else
+        {
+            this.head=node;
+            this.tail=node;
         }
-        this.length++;
+        this.lenght++;
     }
 
     size() {
-        return this.length;
+        if(this.length===0){
+            return 0;
+        }
+        return this.lenght;
     }
 
     at(index) {
-        if(index<0||index>=this.length){
-            return null;
-        }
         var i=0;
-        var a=this.head;
-        while(i<index){
-            a=a.next;
-            i++;
+        var node=this.head;
+        if( this.lenght==0||index>this.lenght||index<0)
+        { return null;
+
         }
-        return a.data;
+        if(index==0)
+            return this.head.data;
+        else
+        {
+            for(;i<index;i++)
+                node=node.next;
+            return node.data;
+        }
     }
 
     findNode(data) {
-        var i=0;
-        var a=this.head;
-        for(var i=0;i<this.length;i++){
-            if(a.data==data)
-                return a;
-            a=a.next;
+        var r = this.head;
+        for(var i=0;i<this.lenght;i++)
+        { if (r.data == data)
+        {
+            return r;
         }
-
+            r = r.next;
+        }
         return null;
     }
 
-
     toArray() {
-        var arr=[];
-        var a=this.head;
-        for(var i=0;a;i++) {
-            arr[i]=a.data;
-            a=a.next;
+        var pushnod=this.head;
+        var masss = [];
+        while (pushnod!= null) {
+            masss.push(pushnod.data);
+            pushnod = pushnod.next;
         }
-        return arr;
+        return masss;
     }
 
     removeAt(index) {
@@ -80,7 +89,7 @@ class SelfOrganizedList {
             {
                 this.head = this.head.next;
                 this.head.prev = null;
-            }else{
+            }
             while(coun<index)
             {
                 coun++;
@@ -89,44 +98,31 @@ class SelfOrganizedList {
             n.next.prev = n.prev;
             n.prev.next = n.next;
         }
-        }
         this.length--;
     }
 
-    moveToFront(node) {
-        if(this.length==0)
-            return;
-
-        var a=this.head;
-        while(a){
-            if(a==node){
-                while(a!=this.head){
-                    var tmp=a.data;
-                    a.data=a.prev.data;
-                    a.prev.data=tmp;
-                    a=a.prev;
-                }
-                return;
-            }
-            a=a.next;
+    moveToFront(node)
+    {
+        var tmp,zn=node.data;
+        while(node!=this.head)
+        {
+            tmp = node.prev.data;
+            node.prev.data = node.data;
+            node.data = tmp;
+            node=node.prev;
         }
+        this.head.data=zn;
     }
 
     reorganize(data) {
-        if(this.length==0)
+        if(this.lenght==0)
             return false;
-
-        var i=0;
-        var a=this.head;
-        for(var i=0;i<this.length;i++){
-            if(a.data==data){
-                this.moveToFront(a);
-                return true;
-            }
-            a=a.next;
+        if(this.findNode(data))
+        {
+            this.moveToFront(this.findNode(data));
+            return true;
         }
-
-        return false;  
+        return false;
     }
 
 }
